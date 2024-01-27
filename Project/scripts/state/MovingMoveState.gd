0f777300a9a86b3355a9bd7d55ba8fbc3611d348
Austin-Continuum <ataiyodexe@gmail.com>
2024-01-27 22:48:54 +0900

    Snake Movement Working

--- Project/scripts/state/MovingMoveState.gd
+++ Project/scripts/state/MovingMoveState.gd
@@ -2,37 +2,54 @@ class_name MovingMoveState
 extends State
 
 @export var player: Player
+@export var movement_interval: float = 1 # In seconds
 
-var input_times: int = 0
+var movement_clock: Timer
+
+func _ready():
+	movement_clock = Timer.new()
+	add_child(movement_clock)
+	movement_clock.one_shot = true
+	movement_clock.autostart = false
+	movement_clock.timeout.connect(_on_movement_tick_timeout)
 
 func Enter():
 	print("Transitioned to Moving State")
-	_move(player.current_direction)
+	_move() # Move immediately first time
+	_tick_movement_clock()
 	pass
+	
+func Exit():
+	print("Exiting MovingMoveState")
+	movement_clock.stop()
 
 func Handle_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
 		# Move up
-		_move(GridUtility.DIRECTIONS.UP)
+		_change_direction(GridUtility.DIRECTIONS.UP)
 	elif event.is_action_pressed("ui_down"):
 		# Move down
-		_move(GridUtility.DIRECTIONS.DOWN)
+		_change_direction(GridUtility.DIRECTIONS.DOWN)
 	elif event.is_action_pressed("ui_left"):
 		# Move left
-		_move(GridUtility.DIRECTIONS.LEFT)
+		_change_direction(GridUtility.DIRECTIONS.LEFT)
 	elif event.is_action_pressed("ui_right"):
 		# Move right
-		_move(GridUtility.DIRECTIONS.RIGHT)
-		
-func Exit():
-	print("Exiting MovingMoveState")
-	input_times = 0
+		_change_direction(GridUtility.DIRECTIONS.RIGHT)
 
-func _move(direction: Vector2):
+func _change_direction(direction: Vector2):
 	player.current_direction = direction
+	transitioned.emit("MovingMoveState")
+
+func _move():
 	var new_position: Vector2 = player.grid_position + player.current_direction
 	player.grid_position = new_position
 	
-	input_times += 1
-	if input_times > 5:
-		transitioned.emit("PausedMoveState")
+func _tick_movement_clock():
+	movement_clock.stop()
+	movement_clock.wait_time = movement_interval
+	movement_clock.start()
+	
+func _on_movement_tick_timeout():
+	_move() # Move
+	_tick_movement_clock() # And start another tick
--- Project/scripts/state/PausedMoveState.gd
+++ Project/scripts/state/PausedMoveState.gd
@@ -1,31 +1,31 @@
 class_name PausedMoveState
 extends State
 
-var timer: Timer
-
-func Enter():
-	print("Transitioned to Paused State")
-	timer = Timer.new()
-	add_child(timer)
-	timer.wait_time = 8 # 8 seconds
-	timer.one_shot = true
-	
-	timer.timeout.connect(_on_Timer_timeout)
-	
-	# Start the timer
-	timer.start()
-	print("Started timer for 8 seconds")
-
-func Exit():
-	print("Exiting PausedMoveState")
-	if timer:
-		print("Removing Timer")
-		timer.queue_free() # This will remove the Timer node safely
-	pass
-	
-
-func _on_Timer_timeout():
-	print("Timer finished!")
-	
-	# Do your timeout logic here
-	transitioned.emit("IdleMoveState")
+#var timer: Timer
+#
+#func Enter():
+	#print("Transitioned to Paused State")
+	#timer = Timer.new()
+	#add_child(timer)
+	#timer.wait_time = 8 # 8 seconds
+	#timer.one_shot = true
+	#
+	#timer.timeout.connect(_on_Timer_timeout)
+	#
+	## Start the timer
+	#timer.start()
+	#print("Started timer for 8 seconds")
+#
+#func Exit():
+	#print("Exiting PausedMoveState")
+	#if timer:
+		#print("Removing Timer")
+		#timer.queue_free() # This will remove the Timer node safely
+	#pass
+	#
+#
+#func _on_Timer_timeout():
+	#print("Timer finished!")
+	#
+	## Do your timeout logic here
+	#transitioned.emit("IdleMoveState")
